9b4d5c00ffbf7449fa9d2a5994e7b4844af345eb
KamillaB-oss <83229084+KamillaB-oss@users.noreply.github.com>
2023-06-28 14:53:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
 from training import get_response
-import nltk
-#nltk.download('punkt')
-from nltk.stem.lancaster import LancasterStemmer
-stemmer = LancasterStemmer()
 
 from flask import Flask, render_template, request, jsonify
 
 from main import get_response
 
+#app wird initialisert
 app = Flask(__name__)
-
+# hier wird die erstellte HTML Datei dem localhost zugewiesen
 @app.route("/")
 def home():
     return render_template('chat.html')
-
+# bei der Eingabe wird hier die programmierte Funktion aus training.py aufgerufen, um die Antwort für die Frage zu erhalten
 @app.route("/chatbot", methods = ["GET", "POST"])
 def chatbot():
     user_input = request.form["message"]
     return str(get_response(str(user_input)))
 
-
+# führt die Webanwendung aus
 if __name__ == '__main__':
     app.run()
